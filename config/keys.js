9c51fa73cd5660d776a1c39056c28a98459f632f
Kaeneth Dayao <kendayao@gmail.com>
2020-11-09 11:53:18 -0800

    change to atlas

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-    mongoURI: "mongodb://user1:password1@ds027175.mlab.com:27175/heroku_5bwdt289",
-    secretOrKey: "password1"
+    mongoURI: "mongodb+srv://fidofriend_user1:"+process.env.DB_PASS+"<password>@fidofriend.pddfr.mongodb.net/fidofriend?retryWrites=true&w=majority",
+    secretOrKey: process.env.KEY
   };
 
   // "mongodb://user1:password1@ds027175.mlab.com:27175/heroku_5bwdt289"
